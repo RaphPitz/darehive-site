1929653169d0cbd63e6530693699421973a01a36
RaphDev <RaphPitz@gmail.com>
2025-05-22 13:15:34 +0200

    fix: correction NextFont .variable pour build Netlify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={"antialiased"}
         style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}
       >
         <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
